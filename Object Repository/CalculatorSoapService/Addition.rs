6a45864abd6e87a90b0a306a613763f60763b7ed
ab250125 <ab250125@WEGAB250125-CS4.TD.TERADATA.COM>
2020-02-09 05:52:10 -0800

    first project commit

---
+++ Object Repository/CalculatorSoapService/Addition.rs
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Addition</name>
+   <tag></tag>
+   <elementGuidId>df390c88-d1e5-4a37-b7ea-aa3df74c5dee</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <restRequestMethod></restRequestMethod>
+   <restUrl></restUrl>
+   <serviceType>SOAP</serviceType>
+   <soapBody>&lt;Envelope xmlns=&quot;http://schemas.xmlsoap.org/soap/envelope/&quot;>
+    &lt;Body>
+        &lt;Add xmlns=&quot;http://tempuri.org/&quot;>
+            &lt;intA>10&lt;/intA>
+            &lt;intB>5&lt;/intB>
+        &lt;/Add>
+    &lt;/Body>
+&lt;/Envelope></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod>SOAP</soapRequestMethod>
+   <soapServiceFunction>Add</soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+
+assertThat(response.getResponseText()).contains('AddResult')
+WS.verifyElementText(response, 'AddResponse.AddResult', '15')
+
+
+WS.verifyElementPropertyValue(response, 'AddResponse.AddResult', '15')</verificationScript>
+   <wsdlAddress>http://www.dneonline.com/calculator.asmx?WSDL</wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/API_TS.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6ac3e510-3cf9-4146-a86a-694002e466b8</testSuiteGuid>
+   <testCaseLink>
+      <guid>bbb8af01-7aa9-49d0-8da4-9d4ba9937360</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTests/AdditionTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>87f13243-486d-4c6f-af4a-aa5c8a8c04fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/CreateUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e4b11327-1b1f-4e33-aaa3-4b38106b08d1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/DeleteUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b47c326a-a739-4949-b128-011081097e6f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/GetUsersTests/GetUsers</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ff8b1302-3049-4bd7-b03d-f5890b121e38</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/GetCustomer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1f51f4ce-ffad-4633-8bf3-be016be59d36</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/UpdateUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>030e0b76-0961-4d83-a242-dc940671c26a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
